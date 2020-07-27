6f4027b9b4bc8b54635af33151add5328fba8bef
Josh Nichols <technicalpickles@github.com>
2020-07-26 22:21:33 -0400

    lint cli

--- cli.js
+++ cli.js
@@ -1,43 +1,42 @@
 #!/usr/bin/env node
-const React = require("react")
-const ink = require("ink")
-const meow = require("meow")
-const fs = require("fs")
+const React = require('react')
+const ink = require('ink')
+const meow = require('meow')
+const fs = require('fs')
 
-App = require("./src/components/App")
+const App = require('./src/components/App')
 
 const cli = meow(`
-	Usage
-		$ reactive-pager <path>
+  Usage
+    $ reactive-pager <path>
 
-	Examples
-		$ reactive-pager foo.txt
-		Hello, Jane
-`);
-
+  Examples
+    $ reactive-pager foo.txt
+    Hello, Jane
+`)
 
 const path = cli.input[0]
 
 // FIXME duped in app.js
-const enterAltScreenCommand = '\x1b[?1049h';
-const leaveAltScreenCommand = '\x1b[?1049l';
-process.stdout.write(enterAltScreenCommand);
+const enterAltScreenCommand = '\x1b[?1049h'
+const leaveAltScreenCommand = '\x1b[?1049l'
+process.stdout.write(enterAltScreenCommand)
 process.on('exit', () => {
-    process.stdout.write(leaveAltScreenCommand);
-});
+  process.stdout.write(leaveAltScreenCommand)
+})
 
 fs.readFile(path, 'utf8', function (err, data) {
   if (err) {
-    return console.log(err);
+    return console.log(err)
   }
 
-	const lines = data.split("\n")
+  const lines = data.split('\n')
 
-	const props = {
-		path: path,
-		lines: lines
-	}
+  const props = {
+    path: path,
+    lines: lines
+  }
 
-	const app = React.createElement(App, props)
-	ink.render(app)
-});
+  const app = React.createElement(App, props)
+  ink.render(app)
+})
